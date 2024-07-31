e46a2f119092a740f2affafccf9f35c536a65a04
mertkesicikan <mertkesicikan@MERTKESICIK07E7>
2024-07-31 12:14:59 +0300

    Add project files.

---
+++ Interfaces/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    class Program
+    { 
+        static void Main(string[] args)
+        {
+            /*PersonManager manager = new PersonManager();
+            Customer customer = new Customer{ Id = 1, FirstName = "Engin", LastName = "Demiroğ", Address = "Ankara"};
+            manager.Add(customer);
+            Student student = new Student {Id=2,FirstName="engin", LastName="demiroğ", Department="ankara" };
+            manager.Add(student);
+            
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add(new SqlSererCustomerDal());
+            */
+            ICustomerDal[] customerDals = new ICustomerDal[2]
+            {
+                new SqlServerCustomerDal(),
+                new OracleCustomerDal(),
+            };
+            foreach (var customerDal in customerDals)
+            {
+                customerDal.Add();
+            }
+            Console.ReadLine();
+
+        }
+
+    }
+    interface IPerson
+    {
+        int Id { get; set; }
+        string FirstName { get; set; }
+        string LastName { get; set; }
+
+    }
+    class Customer : IPerson
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+    }
+    class Student:IPerson
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Department { get; set; }
+    }
+    class PersonManager
+    {
+        public void Add(IPerson person)
+        {
+            Console.WriteLine(person.FirstName);
+        }
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Button[,] buttons = new Button[8, 8];
+            int top = 0;
+            int left = 0;
+            for (int i = 0; i < buttons.GetUpperBound(0); i++)
+            {
+                for (int j = 0; j < buttons.GetUpperBound(1); j++)
+                {
+                    buttons[i, j] = new Button();
+                    buttons[i, j].Width = 50;
+                    buttons[i, j].Height = 50;
+                    buttons[i, j].Left = left;
+                    buttons[i, j].Top = top;
+                    left += 50;
+                    if ((i + j) % 2 == 0)
+                    {
+                        buttons[i, j].BackColor = Color.Black;
+                        Panel yellowPanel = new Panel();
+                        yellowPanel.Width = 25;
+                        yellowPanel.Height = 25;
+                        yellowPanel.BackColor = Color.Yellow;
+                        yellowPanel.Left = (buttons[i, j].Width - yellowPanel.Width)/2;
+                        yellowPanel.Top = (buttons[i, j].Height - yellowPanel.Height) / 2;
+
+                        buttons[i, j].Controls.Add(yellowPanel);
+
+                    }
+                    else
+                    { 
+                    buttons[i, j].BackColor = Color.White;
+                }
+                this.Controls.Add(buttons[i, j]);
+
+            }
+                top += 50;
+                left = 0;
+        }
+        }
+    }
+}
